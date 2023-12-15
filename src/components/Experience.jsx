d2d40df35e5581521dab0ac42ef4fbef06e180f7
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-12-15 13:23:16 +1100

    fixes animation for experience page on large and small screens

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,8 +3,7 @@ import { useInView } from "react-intersection-observer";
 import Button from "./Button";
 
 const Experience = ({ name }) => {
-  const isSmallScreen = window.innerWidth <= 768;
-  const [ref, inView] = useInView({
+  const [ref] = useInView({
     threshold: 0,
     triggerOnce: true,
   });
@@ -24,30 +23,23 @@ const Experience = ({ name }) => {
     animate: { y: 0, opacity: 1 },
   };
 
-  const smallScreenInView = isSmallScreen || inView;
+  const animationProps = {
+    initial: { x: "-10vw", opacity: 0, scale: 0.5 },
+    animate: { x: 0, opacity: 1, scale: 1 }, // Always animate in, regardless of inView or screen size
+    transition: { duration: 0.4, ease: "easeInOut" },
+    whileHover: { scale: 1.05 },
+  };
 
   return (
     <section className="about">
       <div className="aboutContainer container">
         <div className="row">
           {/* Experience Section */}
-          <motion.div
-            className="col-12"
-            ref={ref}
-            initial={{ x: "-10vw", opacity: 0, scale: 0.5 }}
-            animate={smallScreenInView ? { x: 0, opacity: 1, scale: 1 } : { x: "-10vw", opacity: 0, scale: 0.5 }}
-            transition={{ duration: 0.4, ease: "easeInOut" }}
-            whileHover={{ scale: 1.05 }}
-          >
+          <motion.div className="col-12 col-lg-6" ref={ref} {...animationProps}>
             <motion.div className="contentContainer" variants={staggerVariants}>
               {/* Experience content */}
               <motion.h4 variants={paragraphVariants}>Experience</motion.h4>
-              <motion.div
-                className="contentDescription"
-                variants={staggerVariants}
-                initial="initial"
-                animate={smallScreenInView ? "animate" : "initial"}
-              >
+              <motion.div className="contentDescription" variants={staggerVariants} initial="initial" animate="animate">
                 {experienceData.map((item, index) => (
                   <motion.p key={index} variants={paragraphVariants}>
                     <br />
@@ -64,43 +56,24 @@ const Experience = ({ name }) => {
           </motion.div>
 
           {/* Skills Section */}
-          <motion.div
-            className="col-12"
-            ref={ref}
-            initial={{ x: "-10vw", opacity: 0, scale: 0.5 }}
-            animate={smallScreenInView ? { x: 0, opacity: 1, scale: 1 } : { x: "-10vw", opacity: 0, scale: 0.5 }}
-            transition={{ duration: 0.4, ease: "easeInOut" }}
-            whileHover={{ scale: 1.05 }}
-          >
+          <motion.div className="col-12 col-lg-6" ref={ref} {...animationProps}>
             <motion.div className="contentContainer" variants={staggerVariants}>
               {/* Skills content */}
               <motion.h4 variants={paragraphVariants}>Skills</motion.h4>
-              <motion.div
-                className="contentDescription"
-                variants={staggerVariants}
-                initial="initial"
-                animate={smallScreenInView ? "animate" : "initial"}
-              >
+              <motion.div className="contentDescription" variants={staggerVariants} initial="initial" animate="animate">
                 {skillsData.map((category, index) => (
                   <div key={index}>
                     <br />
                     <strong>{category.title}</strong>: <br />
                     {category.skills.map((skill, skillIndex) => (
                       <span key={skillIndex}>
-                        {skillIndex !== category.skills.length - 1
-                          ? `${skill} | `
-                          : skill}
+                        {skillIndex !== category.skills.length - 1 ? `${skill} | ` : skill}
                       </span>
                     ))}
                     <br />
                   </div>
                 ))}
-                <a
-                  href="/projectImages/Kean Bayneslow - Resume.pdf"
-                  download="Kean Bayneslow - Resume.pdf"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href="/projectImages/Kean Bayneslow - Resume.pdf" download="Kean Bayneslow - Resume.pdf" target="_blank" rel="noopener noreferrer">
                   <Button name="Download Resume" />
                 </a>
               </motion.div>
@@ -120,28 +93,32 @@ const experienceData = [
     company: "Sonder",
     location: "Sydney, Australia",
     date: "2022 - 2023",
-    description: "An experienced Supervisor skilled in leadership, problem-solving, and delivering exceptional customer service to consistently exceed expectations.",
+    description:
+      "An experienced Supervisor skilled in leadership, problem-solving, and delivering exceptional customer service to consistently exceed expectations.",
   },
   {
     title: "Partners Manager",
     company: "Helio Restoration",
     location: "Sydney, Australia",
     date: "2019 - 2022",
-    description: "Enhanced business operations by overseeing growth strategies, national supplier panels, contractor training, and project management, with a focus on key account growth and profitability.",
+    description:
+      "Enhanced business operations by overseeing growth strategies, national supplier panels, contractor training, and project management, with a focus on key account growth and profitability.",
   },
   {
     title: "Restoration Manager",
     company: "AJ Grant Group",
     location: "Sydney, Australia",
     date: "2016 - 2019",
-    description: "Project management, stakeholder engagement, and scoping, emphasising quality assurance and disaster response to deliver optimal results for clients.",
+    description:
+      "Project management, stakeholder engagement, and scoping, emphasising quality assurance and disaster response to deliver optimal results for clients.",
   },
   {
     title: "Police Officer",
     company: "WA Police",
     location: "Perth, Australia",
     date: "2009 - 2016",
-    description: "Complex situation management, investigations, emergency response, and community engagement, with a strong background in leading multi-agency search and rescue operations.",
+    description:
+      "Complex situation management, investigations, emergency response, and community engagement, with a strong background in leading multi-agency search and rescue operations.",
   },
 ];
 
